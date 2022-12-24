c43b478577e64814edf8f21ea6ee50247ea2332d
msaqibdev <msaqibdev@gmail.com>
2022-12-24 17:51:57 +0500

    initial commit

--- res/constant.js
+++ res/constant.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=0f9a9573497e413c96b65a12042c79d2"
+export const BASE_URL = "https://newsapi.org/v2"
+export const API_KEY = "0f9a9573497e413c96b65a12042c79d2"
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,76 +1,75 @@
 import { useState, useEffect } from "react";
-import {View, Text, StyleSheet, ActivityIndicator, StatusBar, ScrollView} from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  StatusBar,
+  ScrollView,
+} from "react-native";
 import { PRIMARY_SCREEN_COLOR } from "../../res/colors";
-import {useGlobalContext} from '../../store/context';
+import { useGlobalContext } from "../../store/context";
 import NoInternet from "../components/NoInternet";
 import Swiper from "react-native-swiper";
-import {DecodeTitle, destructuredPostData} from '../../utill/util';
+import { DecodeTitle, destructuredPostData } from "../../utill/util";
 import Card from "../components/Card";
+import { FlashList } from "@shopify/flash-list";
+import { fetchData } from "../../services/getData";
+import { API_KEY } from "../../res/constant";
+import SwiperBanner from "../components/Swiper";
 
 const Home = () => {
-    const [data, setData] = useState([])
-	console.log("TCL: Home -> [data", data)
-    const {isConnected} = useGlobalContext()
-    console.log(isConnected);
+  const [data, setData] = useState([]);
+  console.log("TCL: Home -> [data", data);
+  const { isConnected } = useGlobalContext();
+  console.log(isConnected);
 
-    useEffect(() => {
-        fetchPosts()
-    
-      }, [])
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
-      const fetchPosts = async () => {
-        try{
-            let result = await fetchData("articles", {})
-			console.log("TCL: fetchPosts -> result", result)
-            setData(result.articles.data)
-            setRefresh(false)
-        } catch (error){
-            console.log(fetchPosts);
-        }
-      }
-
+  const fetchPosts = async () => {
+    let result = await fetchData("top-headlines", {
+      apiKey: "0f9a9573497e413c96b65a12042c79d2",
+      country: "us",
+    });
+    console.log("TCL: fetchPosts -> result", result);
+    setData(result.data.articles);
+  };
 
   const renderItem = ({ item }) => {
+    return (
+      <View>
+        <Text>{item.title}</Text>
+      </View>
+    );
+  };
 
-    let {title, urlToImage} = destructuredPostData(item)
-
-    let decodedTitle = DecodeTitle(title.rendered)
-  
-    return  (
-        isConnected ?
-        <ScrollView style = {styles.MainContainer}>
-        <Card/>
-        <View style={styles.ActivityIndicator}>
-        <ActivityIndicator
-        size={'large'}
-        color={PRIMARY_SCREEN_COLOR}
-        />
-    </View> :
-    <Swiper/>
-    </ScrollView> :
-    <View style = {styles.NoInternet}>
-        <NoInternet/>
+  return (
+    <View style={styles.container}>
+      <SwiperBanner data={data} />
+      <FlashList data={data} renderItem={renderItem} estimatedItemSize={100} />
     </View>
-    )
-}
-}
-const styles = StyleSheet.create({
-    MainContainer:{
-        flex: 1,
-        backgroundColor: '#F5F5F5',
-        marginBottom: 50
-    },
-    ActivityInd: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 30
-    },
-    NoInternet:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }    
-})
+  );
+};
 
-export default Home
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    marginBottom: 50,
+  },
+  ActivityInd: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 30,
+  },
+  NoInternet: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Home;
